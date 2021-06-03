e73c88dc1b2388e4551fddc6df708bcfdaa97439
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-06-03 17:49:58 +0530

    Attempt to use closure compiler.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const ClosurePlugin = require('closure-webpack-plugin');
+
 module.exports = {
     pwa: {
         name: 'Piped',
@@ -14,6 +16,21 @@ module.exports = {
                 urlPattern: /\.(?:png|svg|ico)$/,
                 handler: 'CacheFirst',
             }],
+        }
+    },
+    configureWebpack: {
+        optimization: {
+            minimizer: [
+                new ClosurePlugin({ mode: 'AGGRESSIVE_BUNDLE' }, {
+                    // compiler flags here
+                    //
+                    // for debugging help, try these:
+                    //
+                    // formatting: 'PRETTY_PRINT'
+                    // debug: true,
+                    // renaming: false
+                })
+            ]
         }
     }
 }
