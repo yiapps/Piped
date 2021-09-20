4ad1d93e94c55c7addeebb0953fb11d69aa18697
yiapps <61764805+yiapps@users.noreply.github.com>
2021-09-20 09:54:19 -0500

    Update SearchResults.vue

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -100,7 +100,7 @@ export default {
             });
         },
         async updateResults() {
-            document.title = this.$route.query.search_query + " - Piped";
+            document.title = this.$route.query.search_query + " - QuicTube";
             this.results = this.fetchResults().then(json => (this.results = json));
         },
         handleScroll() {
