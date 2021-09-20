94ca5ab305ffb513ac7d9f4c50e19d03431c9432
yiapps <61764805+yiapps@users.noreply.github.com>
2021-09-20 09:55:33 -0500

    Update TrendingPage.vue

--- src/components/TrendingPage.vue
+++ src/components/TrendingPage.vue
@@ -33,7 +33,7 @@ export default {
         });
     },
     activated() {
-        document.title = this.$t("titles.trending") + " - Piped";
+        document.title = this.$t("titles.trending") + " - QuicTube";
         if (this.videos.length > 0) this.updateWatched(this.videos);
     },
     methods: {
